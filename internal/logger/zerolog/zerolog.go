07b891e6b4867702830eb2efe17b8f4bcc1aa907
Artem Dvoretskii <adwski@users.noreply.github.com>
2024-08-28 12:51:52 +0300

    refactor zerologger

--- internal/logger/zerolog/zerolog.go
+++ internal/logger/zerolog/zerolog.go
@@ -15,9 +15,6 @@ func NewLogger(logger zerolog.Logger) *Logger {
 		Logger: logger,
 	}
 }
-func (l *Logger) Trace(msg string, fields ...any) {
-	emit(l.Logger.Trace(), msg, fields...)
-}
 
 func (l *Logger) Error(msg string, fields ...any) {
 	emit(l.Logger.Error(), msg, fields...)
@@ -31,19 +28,26 @@ func (l *Logger) Debug(msg string, fields ...any) {
 	emit(l.Logger.Debug(), msg, fields...)
 }
 
+func (l *Logger) Trace(msg string, fields ...any) {
+	emit(l.Logger.Trace(), msg, fields...)
+}
+
 func emit(ev *zerolog.Event, msg string, fields ...any) {
+	if len(fields)%2 != 0 {
+		fields = fields[:len(fields)-1]
+	}
 	for i := 0; i < len(fields); i += 2 {
 		key, ok := fields[i].(string)
 		if !ok {
-			return
+			continue
 		}
-		switch field := fields[i+1].(type) {
+		switch val := fields[i+1].(type) {
 		case fmt.Stringer:
-			ev = ev.Any(key, field.String())
+			ev = ev.Str(key, val.String())
 		case error:
-			ev = ev.Any(key, field.Error())
+			ev = ev.Err(val)
 		default:
-			ev = ev.Any(key, field)
+			ev = ev.Any(key, val)
 		}
 	}
 	ev.Msg(msg)
