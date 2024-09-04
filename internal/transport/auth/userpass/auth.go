e86e2856371a2e106b752b7c51a557c181b007c8
Artem Dvoretskii <adwski@users.noreply.github.com>
2024-09-04 13:42:58 +0300

    add userpass auth provider

---
+++ internal/transport/auth/userpass/auth.go
@@ -0,0 +1,67 @@
+package userpass
+
+import (
+	"context"
+	"errors"
+	"time"
+
+	"github.com/ydb-platform/ydb-go-genproto/Ydb_Auth_V1"
+	"github.com/ydb-platform/ydb-go-genproto/protos/Ydb_Auth"
+	"google.golang.org/grpc"
+)
+
+const (
+	defaultYDBAuthExpire = 12 * time.Hour
+)
+
+var (
+	ErrLogin           = errors.New("login request failed")
+	ErrLoginUnmarshall = errors.New("login response unmarshall failed")
+)
+
+type (
+	UserPass struct {
+		authSvc Ydb_Auth_V1.AuthServiceClient
+
+		user string
+		pass string
+	}
+	Config struct {
+		Transport grpc.ClientConnInterface
+		Username  string
+		Password  string
+	}
+)
+
+func NewUserPass(cfg Config) *UserPass {
+	auth := &UserPass{
+		authSvc: Ydb_Auth_V1.NewAuthServiceClient(cfg.Transport),
+
+		user: cfg.Username,
+		pass: cfg.Password,
+	}
+
+	return auth
+}
+
+func (up *UserPass) GetToken(ctx context.Context) (token string, expires time.Time, err error) {
+	resp, err := up.authSvc.Login(ctx, &Ydb_Auth.LoginRequest{
+		User:     up.user,
+		Password: up.pass,
+	})
+
+	if err != nil {
+		err = errors.Join(ErrLogin, err)
+		return
+	}
+	var result Ydb_Auth.LoginResult
+	if err = resp.GetOperation().Result.UnmarshalTo(&result); err != nil {
+		err = errors.Join(ErrLoginUnmarshall, err)
+		return
+	}
+
+	token = result.Token
+	expires = time.Now().Add(defaultYDBAuthExpire)
+
+	return
+}
